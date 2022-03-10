a6a68c79f364bb9cfab6944691c6b8a098e7499f
Oscar Ivan Riascos G <osvan202@gmail.com>
2022-03-10 14:06:07 -0500

    Partially complete creation of business contacts

--- src/components/module-request/company/registration/AccordionRegistration.js
+++ src/components/module-request/company/registration/AccordionRegistration.js
@@ -6,6 +6,7 @@ import Typography from "@mui/material/Typography";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import "../StylesCompany.css";
 import RegistrationBasicCompanyData from "./RegistrationBasicCompanyData";
+import RegistrationContactCompany from "./RegistrationContactCompany";
 
 //Component
 const AccordionRegistration = () => {
@@ -27,7 +28,7 @@ const AccordionRegistration = () => {
           id="panel1bh-header"
         >
           {/*Plane 1:*/}
-          <Typography sx={{ flexShrink: 0, textAlign: "Center" }}>
+          <Typography sx={{ flexShrink: 0 }}>
             DATOS GENERALES DE LA ORGANIZACIÓN 
           </Typography>
         </AccordionSummary>
@@ -40,65 +41,17 @@ const AccordionRegistration = () => {
         expanded={expanded === "panel2"}
         onChange={handleChange("panel2")}
       >
+        {/*DATOS DE CONTACTO*/}
         <AccordionSummary
           expandIcon={<ExpandMoreIcon />}
           aria-controls="panel2bh-content"
           id="panel2bh-header"
         >
-          <Typography sx={{ width: "33%", flexShrink: 0 }}>Users</Typography>
-          <Typography sx={{ color: "text.secondary" }}>
-            You are currently not an owner
-          </Typography>
+          <Typography sx={{ flexShrink: 0 }}>DATOS DE CONTACTO</Typography>
         </AccordionSummary>
         <AccordionDetails>
-          <Typography>
-            Donec placerat, lectus sed mattis semper, neque lectus feugiat
-            lectus, varius pulvinar diam eros in elit. Pellentesque convallis
-            laoreet laoreet.
-          </Typography>
-        </AccordionDetails>
-      </Accordion>
-      <Accordion
-        expanded={expanded === "panel3"}
-        onChange={handleChange("panel3")}
-      >
-        <AccordionSummary
-          expandIcon={<ExpandMoreIcon />}
-          aria-controls="panel3bh-content"
-          id="panel3bh-header"
-        >
-          <Typography sx={{ width: "33%", flexShrink: 0 }}>
-            Advanced settings
-          </Typography>
-          <Typography sx={{ color: "text.secondary" }}>
-            Filtering has been entirely disabled for whole web server
-          </Typography>
-        </AccordionSummary>
-        <AccordionDetails>
-          <Typography>
-            Nunc vitae orci ultricies, auctor nunc in, volutpat nisl. Integer
-            sit amet egestas eros, vitae egestas augue. Duis vel est augue.
-          </Typography>
-        </AccordionDetails>
-      </Accordion>
-      <Accordion
-        expanded={expanded === "panel4"}
-        onChange={handleChange("panel4")}
-      >
-        <AccordionSummary
-          expandIcon={<ExpandMoreIcon />}
-          aria-controls="panel4bh-content"
-          id="panel4bh-header"
-        >
-          <Typography sx={{ width: "33%", flexShrink: 0 }}>
-            Personal data
-          </Typography>
-        </AccordionSummary>
-        <AccordionDetails>
-          <Typography>
-            Nunc vitae orci ultricies, auctor nunc in, volutpat nisl. Integer
-            sit amet egestas eros, vitae egestas augue. Duis vel est augue.
-          </Typography>
+          {/*Plane 1: Form for registration the contact of the company*/}
+          <RegistrationContactCompany />
         </AccordionDetails>
       </Accordion>
     </div>
---
+++ src/components/module-request/company/registration/RegistrationContactCompany.js
@@ -0,0 +1,101 @@
+import React from "react";
+import { useState } from "react";
+import { makeStyles } from "@material-ui/core";
+import CssBaseline from "@mui/material/CssBaseline";
+import Box from "@mui/material/Box";
+import Container from "@mui/material/Container";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
+
+import {
+  Radio,
+  RadioGroup,
+  FormControlLabel,
+  FormControl,
+  FormLabel,
+} from "@mui/material";
+
+const RegistrationContactCompany = () => {
+  const useStyles = makeStyles((theme) => ({
+    root: {
+      display: "flex",
+      flexDirection: "column",
+      justifyContent: "center",
+      alignItems: "center",
+      padding: theme.spacing(2),
+
+      "& .MuiTextField-root": {
+        margin: theme.spacing(1),
+        width: "100%",
+      },
+      "& .MuiButtonBase-root": {
+        margin: theme.spacing(2),
+      },
+    },
+  }));
+
+  const classes = useStyles();
+  const [contName, setContName] = useState("");
+  const [contPosition, setContPosition] = useState("");
+  const [contEmail, setContEmail] = useState("");
+  const [contPhone, setContPhone] = useState("");
+
+  /**
+   * This method allow print to console the result of the form
+   * @param {*} e
+   */
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(contName,contPosition,contEmail,contPhone);
+  };
+
+  return (
+    <>
+      <React.Fragment>
+        <CssBaseline />
+        <Container maxWidth="sm">
+          <Box sx={{ bgcolor: "#F2F6FE"}} />
+          <form className={classes.root} onSubmit={handleSubmit}>
+            <TextField
+              label="Nombre contacto directo"
+              variant="outlined"
+              required
+              value={contName}
+              onChange={(e) => setContName(e.target.value)}
+            />
+            <TextField
+              label="Cargo"
+              variant="outlined"
+              required
+              value={contPosition}
+              onChange={(e) => setContPosition(e.target.value)}
+            />
+            <TextField
+              label="E-mail"
+              variant="outlined"
+              type="email"
+              required
+              value={contEmail}
+              onChange={(e) => setContEmail(e.target.value)}
+            />
+
+            <TextField
+              label="Teléfono - Ext-"
+              variant="outlined"
+              required
+              value={contPhone}
+              onChange={(e) => setContPhone(e.target.value)}
+            />
+            <div>
+              <Button type="submit" variant="contained" color="primary">
+                Guardar información.
+              </Button>
+            </div>
+          </form>
+        </Container>
+      </React.Fragment>
+    </>
+  );
+};
+
+export default RegistrationContactCompany;
