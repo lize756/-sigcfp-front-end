a01ac933a6cedcddd0eaf8d94908c765849ac9eb
Oscar Ivan Riascos G <osvan202@gmail.com>
2022-03-10 12:29:39 -0500

    Add new texfield

--- src/components/module-request/company/registration/RegistrationBasicCompanyData.js
+++ src/components/module-request/company/registration/RegistrationBasicCompanyData.js
@@ -1,39 +1,85 @@
 import React from "react";
-import { FormControl, InputLabel, Input, FormHelperText } from "@mui/material";
-import { Grid } from "@mui/material";
+import { useState } from 'react';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import { makeStyles } from '@material-ui/core';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: theme.spacing(2),
+
+    '& .MuiTextField-root': {
+      margin: theme.spacing(1),
+      width: '100%',
+    },
+    '& .MuiButtonBase-root': {
+      margin: theme.spacing(2),
+    },
+  },
+}));
+
+
+
+
 
 const RegistrationBasicCompanyData = () => {
+  const classes = useStyles();
+  // create state variables for each input
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    console.log(firstName, lastName, email, password);
+  };
+
+
   return (
     <>
-      <Grid container spacing={1}>
-        <Grid container item xs={5} spacing={3}>
-          <FormControl>
-            <InputLabel htmlFor="my-input">Email address</InputLabel>
-            <Input id="nit-company" aria-describedby="my-helper-text" />
-            <FormHelperText id="my-helper-text">
-              We'll never share your email.
-            </FormHelperText>
-          </FormControl>{" "}
-        </Grid>
-        <Grid container item xs={5} spacing={3}>
-          <FormControl>
-            <InputLabel htmlFor="my-input">Email address</InputLabel>
-            <Input id="nit-company" aria-describedby="my-helper-text" />
-            <FormHelperText id="my-helper-text">
-              We'll never share your email.
-            </FormHelperText>
-          </FormControl>{" "}
-        </Grid>
-        <Grid container item xs={5} spacing={3}>
-          <FormControl>
-            <InputLabel htmlFor="my-input">Email address</InputLabel>
-            <Input id="nit-company" aria-describedby="my-helper-text" />
-            <FormHelperText id="my-helper-text">
-              We'll never share your email.
-            </FormHelperText>
-          </FormControl>{" "}
-        </Grid>
-      </Grid>
+      <form className={classes.root} onSubmit={handleSubmit}>
+      <TextField
+        label="Nombre de la Empresa "
+        variant="outlined"
+        required
+        value={firstName}
+        onChange={e => setFirstName(e.target.value)}
+      />
+
+      <TextField
+        label="NIT"
+        variant="outlined"
+        required
+        value={lastName}
+        onChange={e => setLastName(e.target.value)}
+      />
+      <TextField
+        label="Email"
+        variant="outlined"
+        type="email"
+        required
+        value={email}
+        onChange={e => setEmail(e.target.value)}
+      />
+      <TextField
+        label="Password"
+        variant="outlined"
+        type="password"
+        required
+        value={password}
+        onChange={e => setPassword(e.target.value)}
+      />
+      <div>
+        <Button type="submit" variant="contained" color="primary">
+          Signup
+        </Button>
+      </div>
+    </form>
     </>
   );
 };
