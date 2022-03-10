bdbdb75e32ef3c989f74885aa3343a81449f7cb6
Oscar Ivan Riascos G <osvan202@gmail.com>
2022-03-10 01:17:19 -0500

    Add Stepper to registration

--- src/config/routes/MainCompany.jsx
+++ src/config/routes/MainCompany.jsx
@@ -1,16 +1,15 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import RequestList from "../../components/module-request/company/RequestList";
 import Home from "../../components/module-request/company/HomeRequest";
+import StepperRegistration from "../../components/module-request/company/StepperRegistration";
 
 const MainCompany = () => {
   return (
     <>
       <Router>
         <Routes>
-          <Route path="/company" element={<Home name ="PORTAL DE SOLICITUDES PRACTICANTES DE LA UNIVERSIDAD ICESI"/>}>
-          <Route path="request" element={<RequestList />} />
-          </Route>
+          <Route path="/company" element={<Home name ="PORTAL DE SOLICITUDES PRACTICANTES DE LA UNIVERSIDAD ICESI"/>}></Route>
+          <Route path="/register" element={<StepperRegistration/>}></Route>
         </Routes>
       </Router>
     </>
