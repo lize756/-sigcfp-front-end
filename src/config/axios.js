90ce6524a27a37aa155a4248c7517e1a75e58d4f
Oscar Ivan Riascos G <osvan202@gmail.com>
2022-03-18 22:10:45 -0500

    Connect the list of careers from the backend

--- src/config/axios.js
+++ src/config/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 let instance = axios.create({
-  baseURL: "http://localhost:3001/",
+  baseURL: "http://localhost:8484/",
   responseType: "json",
   headers: { "X-Requested-Width": "XMLHttpRequest" },
 });
