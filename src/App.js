bf66b9990c0b3256f4db6c01ee44dfa1d2f0f1a2
Oss21 <47828398+Oss21@users.noreply.github.com>
2022-04-13 19:09:31 -0500

    Correct problem realtionated with the list of companies register

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import React from "react";
 import MainCompany from "./config/routes/MainCompany";
+import MainLocation from "./config/routes/MainLocation";
 //Redu
 import { Provider} from "react-redux"
 import store from "../src/components/store/index"
@@ -10,6 +11,7 @@ function App() {
     <Provider store={store}> {/**" All app store"*/}
       <div>
         <MainCompany />
+        <MainLocation/>
       </div>
     </Provider>
   );
