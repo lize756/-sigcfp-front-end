17b2818511ca9e79e3d79da4b729f05d7d076abb
Oss21 <47828398+Oss21@users.noreply.github.com>
2022-04-26 19:01:54 -0500

    Add new elements

--- src/components/global/profile/ProfilePerson.js
+++ src/components/global/profile/ProfilePerson.js
@@ -12,7 +12,6 @@ import {
   IconButton,
   Input,
   InputAdornment,
-  Container,
 } from "@mui/material";
 import BadgeIcon from "@mui/icons-material/Badge";
 import SaveIcon from "@mui/icons-material/Save";
@@ -182,107 +181,67 @@ const ProfilePerson = () => {
 
   return (
     <div>
-      <Container maxWidth="lg" sx={{ m: 4 }}>
-        <Paper sx={{ mt: 2 }}>
-          <Stack direction="row" spacing={2}>
-            <Avatar
-              sx={{
-                mt: 2,
-                ml: 7,
-                mb: 2,
-                bgcolor: green[500],
-                width: 65,
-                height: 65,
-              }}
-            >
-              <BadgeIcon sx={{ width: 40, height: 40 }} />
-            </Avatar>
+      <Paper sx={{ mt: 2, ml: 5, mr: 5 }}>
+        <Stack direction="row" spacing={2}>
+          <Avatar
+            sx={{
+              mt: 2,
+              ml: 7,
+              mb: 2,
+              bgcolor: green[500],
+              width: 65,
+              height: 65,
+            }}
+          >
+            <BadgeIcon />
+          </Avatar>
 
-            <Box md={6}>
-              <Typography
-                mt={2}
-                ml={2}
-                variant="h6"
-                sx={{ fontWeight: "medium", color: "#072079" }}
-              >
-                {person.persFirstName + " " + person.persLastName}
-              </Typography>
-              <Typography
-                ml={2}
-                mb={2}
-                variant="subtitle2"
-                sx={{ color: "#072079" }}
-              >
-                Editar Perfil
-              </Typography>
-            </Box>
-          </Stack>
-        </Paper>
-
-        <Paper sx={{ mt: 3 }}>
           <Box md={6}>
-            <Stack spacing={3} sx={{ m: 4 }}>
-              <Stack
-                direction={{ xs: "column", sm: "row" }}
-                spacing={3}
-                sx={{ mt: 4 }}
-              >
-                <TextField
-                  sx={{ width: "95%" }}
-                  label="Nombres"
-                  name="persFirstName"
-                  defaultValue={person.persFirstName}
-                  variant="standard"
-                  onChange={handleChange}
-                />
+            <Typography
+              mt={2}
+              ml={2}
+              variant="h6"
+              sx={{ fontWeight: "medium", color: "#072079" }}
+            >
+              {person.persFirstName + " " + person.persLastName}
+            </Typography>
+            <Typography
+              ml={2}
+              mb={2}
+              variant="subtitle2"
+              sx={{ color: "#072079" }}
+            >
+              Editar Perfil
+            </Typography>
+          </Box>
+        </Stack>
+      </Paper>
 
-                <TextField
-                  label="Apellidos"
-                  sx={{ width: "95%" }}
-                  name="persLastName"
-                  defaultValue={person.persLastName}
-                  variant="standard"
-                  onChange={handleChange}
-                />
-              </Stack>
-              <Stack direction={{ xs: "column", sm: "row" }} spacing={3}>
-                <TextField
-                  sx={{ width: "95%" }}
-                  label="Documento de identidad"
-                  name="persDocument"
-                  defaultValue={person.persDocument}
-                  variant="standard"
-                  onChange={handleChange}
-                />
-                <TextField
-                  sx={{ width: "95%" }}
-                  type="email"
-                  label="Correo electrónico"
-                  name="persEmail"
-                  defaultValue={person.persEmail}
-                  variant="standard"
-                  onChange={handleChange}
-                />
-              </Stack>
-              <Stack direction={{ xs: "column", sm: "row" }} spacing={3}>
-                <TextField
-                  sx={{ width: "95%" }}
-                  label="Dirección"
-                  name="persAddress"
-                  defaultValue={person.persAddress}
-                  variant="standard"
-                  onChange={handleChange}
-                />
+      <Paper sx={{ mt: 3, ml: 5, mr: 5 }}>
+        <Box md={6}>
+          <Stack spacing={3} sx={{ m: 4 }}>
+            <Stack
+              direction={{ xs: "column", sm: "row" }}
+              spacing={3}
+              sx={{ mt: 4 }}
+            >
+              <TextField
+                sx={{ width: "95%" }}
+                label="Nombres"
+                name="persFirstName"
+                defaultValue={person.persFirstName}
+                variant="standard"
+                onChange={handleChange}
+              />
 
-                <TextField
-                  sx={{ width: "95%" }}
-                  label="Generó"
-                  name="persGenre"
-                  defaultValue={person.persGenre}
-                  variant="standard"
-                  onChange={handleChange}
-                />
-              </Stack>
+              <TextField
+                label="Apellidos"
+                sx={{ width: "95%" }}
+                name="persLastName"
+                defaultValue={person.persLastName}
+                variant="standard"
+                onChange={handleChange}
+              />
             </Stack>
             <Stack direction={{ xs: "column", sm: "row" }} spacing={3}>
               <TextField
@@ -380,21 +339,123 @@ const ProfilePerson = () => {
                 onChange={handleChange}
               />
             </Stack>
-          </Box>
+          </Stack>
           <Box>
             <Button
               sx={{ mb: 4, ml: 4 }}
               variant="contained"
               startIcon={<SaveIcon />}
               onClick={(e) => {
-                savePassword(e);
+                savePerson(e);
               }}
             >
               Guardar
             </Button>
           </Box>
-        </Paper>
-      </Container>
+        </Box>
+      </Paper>
+
+      <Paper sx={{ mt: 3, ml: 5, mr: 5 }}>
+        <Typography
+          mt={2}
+          ml={2}
+          variant="h6"
+          sx={{ fontWeight: "medium", color: "#072079" }}
+        >
+          Cambiar contraseña
+        </Typography>
+        <Stack spacing={3} sx={{ m: 4 }}>
+          <Stack direction={{ xs: "column", sm: "row" }} spacing={3}>
+            <FormControl sx={{ width: "40ch" }} variant="standard">
+              <InputLabel htmlFor="standard-adornment-password">
+                Contraseña Actual
+              </InputLabel>
+              <Input
+                type={currentPassword.showPassword ? "text" : "password"}
+                value={currentPassword.password}
+                name="password"
+                onChange={handleCurrentPassword}
+                endAdornment={
+                  <InputAdornment position="end">
+                    <IconButton
+                      aria-label="toggle password visibility"
+                      onClick={handleClickShowPassword}
+                    >
+                      {currentPassword.showPassword ? (
+                        <VisibilityOff />
+                      ) : (
+                        <Visibility />
+                      )}
+                    </IconButton>
+                  </InputAdornment>
+                }
+              />
+            </FormControl>
+            <FormControl sx={{ m: 2, width: "40ch" }} variant="standard">
+              <InputLabel htmlFor="standard-adornment-password">
+                Nueva Contraseña
+              </InputLabel>
+              <Input
+                type={newPassword.showPassword ? "text" : "password"}
+                value={newPassword.password}
+                name="password"
+                onChange={handleNewPassword}
+                endAdornment={
+                  <InputAdornment position="end">
+                    <IconButton
+                      aria-label="toggle password visibility"
+                      onClick={handleClickShowNewPassword}
+                    >
+                      {newPassword.showPassword ? (
+                        <VisibilityOff />
+                      ) : (
+                        <Visibility />
+                      )}
+                    </IconButton>
+                  </InputAdornment>
+                }
+              />
+            </FormControl>
+            <FormControl sx={{ m: 2, width: "40ch" }} variant="standard">
+              <InputLabel htmlFor="standard-adornment-password">
+                Confirmar Nueva Contraseña
+              </InputLabel>
+              <Input
+                type={confirmPassword.showPassword ? "text" : "password"}
+                value={confirmPassword.password}
+                name="password"
+                onChange={handleConfirmPassword}
+                endAdornment={
+                  <InputAdornment position="end">
+                    <IconButton
+                      aria-label="toggle password visibility"
+                      onClick={handleClickShowConfirmPassword}
+                    >
+                      {confirmPassword.showPassword ? (
+                        <VisibilityOff />
+                      ) : (
+                        <Visibility />
+                      )}
+                    </IconButton>
+                  </InputAdornment>
+                }
+              />
+            </FormControl>
+          </Stack>
+        </Stack>
+        <Box>
+          <Button
+            sx={{ mb: 4, ml: 4 }}
+            variant="contained"
+            startIcon={<SaveIcon />}
+            onClick={(e) => {
+              savePassword(e);
+            }}
+          >
+            Guardar
+          </Button>
+        </Box>
+      </Paper>
     </div>
   );
 };
